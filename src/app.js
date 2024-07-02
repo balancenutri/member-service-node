bcbd75cc6b38cda9273e9957fd5af51cba439f01
Sumedhp21 <sumedhpawar8966@gmail.com>
2024-07-02 11:00:42 +0530

    Added / route

--- src/app.js
+++ src/app.js
@@ -21,7 +21,9 @@ app.use(
   })
 );
 app.use(morgan("dev"));
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.post("/token", async (req, res) => {
   const { identity } = req.body;
   try {
